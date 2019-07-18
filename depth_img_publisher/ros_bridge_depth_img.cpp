831e523a0d93b2172333e91556dab7f91b3c70b8
david2611 <david.r.hall.2611@gmail.com>
2019-07-18 14:10:51 +1000

    Depth image publisher base functionality achieved. Need to improve header

---
+++ depth_img_publisher/ros_bridge_depth_img.cpp
@@ -0,0 +1,86 @@
+#include "ros_bridge_depth_img.hpp"
+
+//#include "engine/core/assert.hpp"
+
+#include "ros/ros.h"
+
+// needed for the image transport specifically
+#include "image_transport/image_transport.h"
+#include "cv_bridge/cv_bridge.h"
+#include <sensor_msgs/image_encodings.h>
+
+// not sure why these aren't needed (opencv in particular)
+// #include "opencv2/imgcodecs.hpp"
+// #include "opencv2/imgproc.hpp"
+// #include <sensor_msgs/Image.h>
+// #include <sensor_msgs/image_encodings.h>
+
+namespace isaac {
+namespace rosbridge {
+namespace opencv {
+
+// Internal struct for holding the ROS node handle and the publisher/ImageTransport components
+struct DepthImageRosBridge::RosDepthImageData {
+  ros::NodeHandle node;
+  image_transport::Publisher pub;
+  image_transport::ImageTransport it = image_transport::ImageTransport(this->node);
+};
+
+void DepthImageRosBridge::start() {
+  // Setup the ros node and communication channels
+  // Make sure to disable the Sigint handler and if any args are needed
+  // add them to the args string.
+  ros::M_string args;
+  if (!ros::isInitialized()) {
+    ros::init(args, "isaacRosBridge", ros::init_options::NoSigintHandler);
+  }
+
+  // Initialise data struct and define what topic node is publishing on for ROS
+  depth_img_data_ = std::make_unique<RosDepthImageData>();
+  depth_img_data_->pub = depth_img_data_->it.advertise(
+      get_ros_publisher_channel_name(), get_ros_publisher_queue_size());
+
+  //Tick on message since only publishing. If subscribing need to tick periodically
+  tickOnMessage(rx_depth_image());
+}
+
+void DepthImageRosBridge::tick() {
+  if (ros::ok()) {
+
+    // parse the received messages
+    auto proto1 = rx_depth_image().getProto();
+    ImageConstView1f depth_image;
+    bool ok = FromProto(proto1.getDepthImage(), rx_depth_image().buffers(), depth_image);
+    
+    if (ok) {
+        // convert the Image Proto into a CV Mat
+        const size_t rows = depth_image.rows();
+        const size_t cols = depth_image.cols();
+        cv::Mat cv_depth_image = cv::Mat(rows, cols, CV_32FC1,
+                        const_cast<void*>(static_cast<const void*>(depth_image.data().pointer())));
+        
+        // convert cv Mat to  an image pointer message with cv_bridge
+        // need to update the header?
+        sensor_msgs::ImagePtr ros_depth_img_msg = cv_bridge::CvImage(std_msgs::Header(), "32FC1", cv_depth_image).toImageMsg();
+        depth_img_data_->pub.publish(ros_depth_img_msg);
+
+    }
+  } else {
+    LOG_ERROR("An error has occurred within ROS.");
+  }
+}
+
+void DepthImageRosBridge::stop() {
+  depth_img_data_->pub.shutdown();
+  depth_img_data_ = nullptr;
+}
+
+DepthImageRosBridge::~DepthImageRosBridge() {
+}
+
+DepthImageRosBridge::DepthImageRosBridge() {
+}
+
+}  // namespace opencv
+}  // namespace rosbridge
+}  // namespace isaac
