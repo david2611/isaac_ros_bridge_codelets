7d2fff2c6cd4ce33e27705f57f3e7c92224a5515
david2611 <david.r.hall.2611@gmail.com>
2019-09-05 15:15:45 +1000

    First rough version of robot transform tree working

---
+++ robot_tf_publisher/ros_bridge_robot_tf.cpp
@@ -0,0 +1,93 @@
+#include "ros_bridge_robot_tf.hpp"
+
+
+#include "ros/ros.h"
+#include "tf/transform_broadcaster.h"
+
+namespace isaac {
+namespace rosbridge {
+
+void print_pose(Pose3d pose){ 
+  auto translation = pose.translation;
+  auto rpy = pose.rotation.eulerAnglesRPY();
+  auto quaternion = pose.rotation.quaternion();
+  // print translation
+  std::cout << "Translation: {X: " << translation[0] << ", Y: " << translation[1] ;
+  std::cout << ", Z: " << translation[2] << "}\n";
+  // print rotation roll pitch yaw
+  std::cout << "RPY: {Roll: " << rpy[0] << ", Pitch: " << rpy[1] << ", Yaw: ";
+  std::cout << rpy[2] << "}\n";
+  // print rotation quaternion
+  std::cout << "Quaternion: {W: " << quaternion.w() << ", X: " << quaternion.x() << ", Y: ";
+  std::cout << quaternion.y() << ", Z: " << quaternion.z() << "}\n";
+}
+
+// Internal struct for holding the ROS node handle and the publisher/ImageTransport components
+struct RobotTFRosBridge::RosRobotTFData {
+  ros::NodeHandle node;
+  tf::TransformBroadcaster tf_broadcaster;
+};
+
+void RobotTFRosBridge::start() {
+  // Setup the ros node and communication channels
+  // Make sure to disable the Sigint handler and if any args are needed
+  // add them to the args string.
+  ros::M_string args;
+  if (!ros::isInitialized()) {
+    ros::init(args, "isaacRosBridge", ros::init_options::NoSigintHandler);
+  }
+
+  // Initialise data struct and define what topic node is publishing on for ROS
+  robot_tf_data_ = std::make_unique<RosRobotTFData>();
+
+  //Tick Periodically and very rarely (should not change but update to be sure)
+  tickPeriodically();
+}
+
+void RobotTFRosBridge::tick() {
+  if (ros::ok()) {
+    std::string robot_name = get_isaac_robot_name();
+    std::vector<std::string> components = get_isaac_robot_components();
+    double tick_time = getTickTime();
+    double node_clock_timestamp = node()->clock()->timestamp();
+    ros::Time ros_tick_time = ros::Time::now();
+    std::cout << components.size() << std::endl;
+    for (uint i=0; i < components.size(); i++)
+    {
+      std::cout << robot_name << ", " << components[i] << std::endl;
+      std::cout << tick_time << std::endl;
+      std::cout << node_clock_timestamp << std::endl;
+
+      // get the pose tree transform for the robot to the component
+      std::optional<Pose3d> robotTcomp = node()->pose().tryGet(robot_name, components[i], tick_time);
+
+      
+      if (robotTcomp){
+        tf::Transform transform;
+        // print the pose tree transform info (temp)
+        print_pose(*robotTcomp);
+        // create transform and broadcast it
+        auto translation = robotTcomp->translation;
+        auto quaternion = robotTcomp->rotation.quaternion();
+        transform.setOrigin(tf::Vector3(translation[0], translation[1], translation[2]));
+        transform.setRotation(tf::Quaternion(quaternion.w(), quaternion.x(), quaternion.y(), quaternion.z()));
+        robot_tf_data_->tf_broadcaster.sendTransform(tf::StampedTransform(transform, ros_tick_time, robot_name, robot_name + "_" + components[i]));
+      }
+    }
+  } else {
+    LOG_ERROR("An error has occurred within ROS.");
+  }
+}
+
+void RobotTFRosBridge::stop() {
+  robot_tf_data_ = nullptr;
+}
+
+RobotTFRosBridge::~RobotTFRosBridge() {
+}
+
+RobotTFRosBridge::RobotTFRosBridge() {
+}
+
+}  // namespace rosbridge
+}  // namespace isaac
---
+++ robot_tf_publisher/ros_bridge_robot_tf.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "engine/alice/alice.hpp"
+#include "messages/messages.hpp"
+
+namespace isaac {
+namespace rosbridge {
+
+// This codelet represents a basic bridge to ROS for publishing images.
+// Every time an image is available it will convert it to a CV Mat object and use cv_bridge
+// to publish the Image message to ROS
+class RobotTFRosBridge : public alice::Codelet {
+ public:
+  // Explicitly declare constructors and destructors
+  // to get around forward declaration of RosNavigationData
+  RobotTFRosBridge();
+  virtual ~RobotTFRosBridge();
+
+  void start() override;
+  void tick() override;
+  void stop() override;
+
+  // Name of robot in Isaac PoseTree
+  ISAAC_PARAM(std::string, isaac_robot_name, "robot");
+
+  // Name of all components (sensors) on the robot with available poses
+  ISAAC_PARAM(std::vector<std::string>, isaac_robot_components, {});
+
+
+ private:
+  // Hide the ROS implementation details
+  struct RosRobotTFData;
+  std::unique_ptr<RosRobotTFData> robot_tf_data_;
+};
+
+}  // namespace rosbridge
+}  // namespace isaac
+
+ISAAC_ALICE_REGISTER_CODELET(isaac::rosbridge::RobotTFRosBridge);
