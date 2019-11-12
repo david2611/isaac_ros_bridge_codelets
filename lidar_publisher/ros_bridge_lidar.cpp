539848e09b1194bbd2cafc996b2a51364e8377c7
david2611 <david.r.hall.2611@gmail.com>
2019-11-12 10:24:18 +1000

    Updated naming conventions, added example app, created readme

--- lidar_publisher/ros_bridge_lidar.cpp
+++ lidar_publisher/ros_bridge_lidar.cpp
@@ -1,4 +1,4 @@
-#include "lidar_publisher.hh"
+#include "ros_bridge_lidar.hpp"
 
 #include <algorithm>
 #include <fstream>
@@ -20,12 +20,12 @@ inline double wrap_angle_to_pi(const double angle) {
   return a;
 }
 
-struct LidarPublisher::RosData {
+struct LidarRosBridge::RosData {
   ros::NodeHandle nh;
   ros::Publisher pub;
 };
 
-void LidarPublisher::start() {
+void LidarRosBridge::start() {
   // Start our monolothic ROS node if it doesn't already exist
   if (!ros::isInitialized())
     ros::init(ros::M_string(), "benchbot_simulator",
@@ -34,19 +34,19 @@ void LidarPublisher::start() {
   // Initialise all of the ROS data we are going to need
   ros_data_ = std::make_unique<RosData>();
   ros_data_->pub = ros_data_->nh.advertise<sensor_msgs::LaserScan>(
-      get_lidar_channel_name(), 2);
+      get_lidar_ros_topic_name(), 2);
 
   // Configure the codelet to only tick when we receive a new camera message
   // from the simulator
   tickOnMessage(rx_lidar_scan());
 }
 
-void LidarPublisher::stop() {
+void LidarRosBridge::stop() {
   ros_data_->pub.shutdown();
   ros_data_ = nullptr;
 }
 
-void LidarPublisher::tick() {
+void LidarRosBridge::tick() {
   if (ros::ok()) {
     // Received a message, cache time ASAP
     ros::Time msg_time = ros::Time::now();
@@ -55,7 +55,7 @@ void LidarPublisher::tick() {
     auto lidar_proto = rx_lidar_scan().getProto();
     sensor_msgs::LaserScan lidar_ros;
     lidar_ros.header.stamp = msg_time;
-    lidar_ros.header.frame_id = get_lidar_frame_name();
+    lidar_ros.header.frame_id = get_lidar_ros_frame_name();
     lidar_ros.angle_min = *(lidar_proto.getAngles().end() - 1);
     lidar_ros.angle_max = *lidar_proto.getAngles().begin();
     lidar_ros.angle_increment =
@@ -64,7 +64,9 @@ void LidarPublisher::tick() {
     lidar_ros.range_max = lidar_proto.getOutOfRangeThreshold();
     for (const double &r : lidar_proto.getRanges())
       lidar_ros.ranges.push_back(r);
-    std::reverse(lidar_ros.ranges.begin(), lidar_ros.ranges.end());
+
+    // reverse needed for Ben setup. Not sure why yet.
+    // std::reverse(lidar_ros.ranges.begin(), lidar_ros.ranges.end());
 
     // Publish the ROS LaserScan message
     ros_data_->pub.publish(lidar_ros);
